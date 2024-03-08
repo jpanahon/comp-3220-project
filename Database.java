d3de02d2cccebe31b7f94fd9da9c542440ed8802
jpanahon <36034467+jpanahon@users.noreply.github.com>
2024-03-08 18:51:40 -0500

    added code and class outline

---
+++ Database.java
@@ -0,0 +1,17 @@
+public class Database {
+    public void queryData() {
+
+    }
+
+    public void deleteData() {
+
+    }
+
+    public void insertData() {
+
+    }
+
+    public void updateData() {
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,5 @@
+class WindsorOpenData {
+    public static void main(String[] args) {
+        System.out.println("Main Entry Point");
+    }
+}
